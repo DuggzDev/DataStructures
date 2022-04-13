f5671b0d51c5219b2c5d180b0426225bc895c196
Duggz <100001472+DuggzDev@users.noreply.github.com>
2022-04-13 11:37:18 +0100

    Update GraphMatrix.java

--- DS/src/main/java/DSA/GraphMatrix.java
+++ DS/src/main/java/DSA/GraphMatrix.java
@@ -55,6 +55,22 @@ for(int i = 0; i < matrix.length; i++){
             if(matrix[src][i] == 1{
                 dfsHelper(i, visited);
         }
+   public void breadthFirstSearch(int src){
+       Quere<Integer> queue = new LinkedList<>();
+       boolean[] visited = new boolean[matrix.length];
+       queue.offer(src);
+       visited[src] = true;
+       while(queue.size() != 0){
+           src = queue.poll();
+           System.out.println(nodes.get(src).data + " = visited");
+           for(int i =0; i < matrix[src].length;i++{
+               if(matrix[src][i] == 1 && !visited[i])
+               {
+                   queue.offer(i);
+                   visited[i] =true;
+               }
+           }
+               }
 }
 class Node{
     char data;
